6305d772c62b8c6e451ddd10cf73201854c73d86
qminhminh <hqminh050503@gmail.com>
2024-07-21 16:55:43 +0700

    color

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -4,9 +4,9 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 String googleApiKey = 'AIzaSyAYgK10l_C_HG-pSeVBVUxChyGSm6wa78Q';
 
-const kPrimary = Color(0xFF30b9b2);
+const kPrimary = Color.fromARGB(255, 240, 185, 96);
 const kPrimaryLight = Color(0xFF40F3EA);
-const kSecondary = Color(0xffffa44f);
+const kSecondary = Color.fromARGB(255, 163, 83, 7);
 const kSecondaryLight = Color(0xFFffe5db);
 const kTertiary = Color(0xff0078a6);
 const kGray = Color(0xff83829A);
